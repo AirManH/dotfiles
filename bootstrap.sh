fcaaafca60fc6657bbf2ec15a112149b18146228
air <airmanhigh@gmail.com>
2020-09-21 23:23:04 +0800

    feat: backup the old .zshrc if it exists

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,9 @@
 cd "$(dirname "$0")"
 DOTFILES_ROOT=$(pwd -P)
 
+
+# backup if the old one exists
+if [ -f ${HOME}/.zshrc ]; then
+  mv ${HOME}/.zshrc ${HOME}/.zshrc.old
+fi
 cp ${DOTFILES_ROOT}/zsh/.zshrc ${HOME}/.zshrc
